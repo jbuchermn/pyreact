bbbd03cd4919e0a4fedae2fb38df028b0047e92a
Jonas Bucher <j.bucher.mn@gmail.com>
2020-11-02 22:23:54 +0100

    Minor

--- py/app.py
+++ py/app.py
@@ -5,6 +5,7 @@ from abc import abstractmethod
 
 SOCKET_PORT = 8081
 
+
 class App:
     def __init__(self, event_loop):
         self._event_loop = event_loop
@@ -24,6 +25,11 @@ class App:
             if result is not None:
                 await self._client_socket.send(json.dumps(result))
 
+    async def render(self):
+        result = self._process({})
+        if result is not None and self._client_socket is not None:
+            await self._client_socket.send(json.dumps(result))
+
     def _wrap(self, py_obj):
         if callable(py_obj):
             self._funcs_idx += 1
@@ -37,12 +43,14 @@ class App:
             return {k: self._wrap(py_obj[k]) for k in py_obj.keys()}
 
         elif hasattr(py_obj, '__dict__'):
-            res = {k: self._wrap(py_obj.__dict__[k]) for k in py_obj.__dict__.keys()}
+            res = {k: self._wrap(py_obj.__dict__[k])
+                   for k in py_obj.__dict__.keys()}
             for m in py_obj.__class__.__dict__.keys():
                 if not m.startswith("__"):
                     def make(func):
                         def call(*args, **kwargs):
-                            py_obj.__class__.__dict__[func](py_obj, *args, **kwargs)
+                            py_obj.__class__.__dict__[func](
+                                py_obj, *args, **kwargs)
                         return call
                     res[m] = self._wrap(make(m))
             return res
@@ -50,32 +58,24 @@ class App:
         else:
             return py_obj
 
-    def _unwrap(self, js_obj):
-        if isinstance(js_obj, list):
-            return [self._unwrap(o) for o in js_obj]
-
-        elif isinstance(js_obj, dict):
-            return {k: self._unwrap(js_obj[k]) for k in js_obj.keys()}
-
-        else:
-            return js_obj
-
     def _process(self, data):
-        if data['__pyreact_kind'] == 'func_call':
+        if '__pyreact_kind' in data and data['__pyreact_kind'] == 'func_call':
             if data['__pyreact_id'] not in self._funcs:
-                print("[debug] %s not in %s" % (data['__pyreact_id'], self._funcs.keys()))
+                print("[debug] cannot call %s, available: %s" %
+                      (data['__pyreact_id'], self._funcs.keys()))
             else:
                 self._funcs[data['__pyreact_id']](
-                    *self._unwrap(data['__pyreact_args']))
+                    *data['__pyreact_args'])
 
-        props = self.render()
+        self._funcs = {}
+        props = self._render()
         return {
             'props': self._wrap(props),
             '__pyreact_kind': 'render'
         }
 
     @abstractmethod
-    def render(self):
+    def _render(self):
         pass
 
 
@@ -89,12 +89,13 @@ class DummyData:
     def dec(self):
         self.counter -= 1
 
+
 class AppImpl(App):
     def __init__(self, event_loop):
         super().__init__(event_loop)
         self.dummy = DummyData(0)
 
-    def render(self):
+    def _render(self):
         return {
             'dummy': self.dummy,
         }
